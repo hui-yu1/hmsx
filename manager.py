96c1b9812b33522481184b0b9a98ab25a477a877
huiyu <3478474830@qq.com>
2020-04-23 14:59:15 +0800

    login.js

--- manager.py
+++ manager.py
@@ -2,8 +2,13 @@ from application import app,manager
 from flask_script import Server
 import urls
 
+<<<<<<< HEAD
 # web服务器配置
 manager.add_command("runserver",Server(host="localhost",port=5000,use_debugger=True,use_reloader=True))
+=======
+# web 服务器
+manager.add_command( "runserver",Server(host="localhost",port=5000,use_debugger=True,use_reloader=True) )
+>>>>>>> 6c785b6... login
 
 def main():
     manager.run()
--- web/controllers/user/User.py
+++ web/controllers/user/User.py
@@ -1,7 +1,17 @@
+<<<<<<< HEAD
 from flask import Blueprint
+=======
+from flask import Blueprint,render_template
+
+>>>>>>> 6c785b6... login
 
 router_user = Blueprint('user_page',__name__)
 
 @router_user.route("/login")
+<<<<<<< HEAD
 def index():
     return "ok"
+=======
+def login():
+    return render_template('user/login.html')
+>>>>>>> 6c785b6... login
